cc8d9974f8655fdb06e6affa3be4f6906e7379cb
Johanna Mantilla Duque <johanna1431@gmail.com>
2012-10-30 13:22:29 -0500

    Load migrations in test environment
    Signed-off-by: Johanna Mantilla Duque <johanna1431@gmail.com>

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,6 +5,7 @@ require "rails_app/config/environment"
 
 require 'rspec/rails'
 require 'devise-authy'
+require 'orm/active_record'
 require 'database_cleaner'
 
 # Requires supporting files with custom matchers and macros, etc,
