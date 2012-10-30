7c76e7f2898dfc98b4801a364118f6afa22a1e62
Johanna Mantilla Duque <johanna1431@gmail.com>
2012-10-30 00:15:24 -0500

    Add rails_app for tests
    Signed-off-by: Johanna Mantilla Duque <johanna1431@gmail.com>

---
+++ spec/rails_app/app/models/user.rb
@@ -0,0 +1,9 @@
+class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :token_authenticatable, :encryptable, :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :authy_authenticatable, :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  # Setup accessible (or protected) attributes for your model
+  attr_accessible :email, :password, :password_confirmation, :authy_id, :last_sign_in_with_authy
+end
---
+++ spec/rails_app/config/initializers/authy.rb
@@ -0,0 +1,3 @@
+require 'authy'
+
+Authy.api_key = 'faa790e925ddebed1288d2dd28242291'
