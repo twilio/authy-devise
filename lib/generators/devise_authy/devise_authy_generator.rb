04c2513faccfa0921487a960745d52336f55c381
David A. Cuadrado <krawek@gmail.com>
2013-04-09 14:53:17 -0500

    Make the generator idempotent.
    So we can run it several times on the same model without any trouble.

--- lib/generators/devise_authy/devise_authy_generator.rb
+++ lib/generators/devise_authy/devise_authy_generator.rb
@@ -1,19 +1,30 @@
 module DeviseAuthy
-	module Generators
-		class DeviseAuthyGenerator < Rails::Generators::NamedBase
+  module Generators
+    class DeviseAuthyGenerator < Rails::Generators::NamedBase
 
-			namespace "devise_authy"
+      namespace "devise_authy"
 
-			desc "Add :authy_authenticatable directive in the given model, plus accessors. Also generate migration for ActiveRecord"
+      desc "Add :authy_authenticatable directive in the given model, plus accessors. Also generate migration for ActiveRecord"
 
-			def inject_devise_authy_content
-				path = File.join("app","models","#{file_path}.rb")
-				inject_into_file(path, "authy_authenticatable, :", :after => "devise :") if File.exists?(path)
-				inject_into_file(path, ":authy_id, :last_sign_in_with_authy", :after => "attr_accessible ") if File.exists?(path)
-			end
+      def inject_devise_authy_content
+        path = File.join("app","models","#{file_path}.rb")
+        if File.exists?(path) &&
+          !File.read(path).include?("authy_authenticatable")
+          inject_into_file(path,
+                           "authy_authenticatable, :",
+                           :after => "devise :")
+        end
 
-			hook_for :orm
+        if File.exists?(path) &&
+          !File.read(path).include?(":authy_id")
+          inject_into_file(path,
+                           ":authy_id, :last_sign_in_with_authy, ",
+                           :after => "attr_accessible ") 
+        end
+      end
 
-		end
-	end
+      hook_for :orm
+
+    end
+  end
 end
