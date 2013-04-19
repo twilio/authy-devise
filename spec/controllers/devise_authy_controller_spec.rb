6783f854a1669214569014dae60a7a1de5e57284
Johanna Mantilla Duque <johanna1431@gmail.com>
2013-04-19 15:23:37 -0500

    Updated and added specs for devise_authy_controller

--- spec/controllers/devise_authy_controller_spec.rb
+++ spec/controllers/devise_authy_controller_spec.rb
@@ -4,82 +4,141 @@ describe Devise::DeviseAuthyController do
   include Devise::TestHelpers
 
   before :each do
-    @user = create_user(:authy_id => '80')
+    request.env["devise.mapping"] = Devise.mappings[:user]
+    @user = create_user(:authy_id => 2)
   end
 
-  describe "GET #show" do
+  describe "GET #verify_authy" do
     it "Should render the second step of authentication" do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      get :show
-      response.should render_template('show')
+      request.session["user_id"] = @user.id
+      request.session["user_password_checked"] = true
+      get :GET_verify_authy
+      response.should render_template('verify_authy')
+    end
+
+    it "Should no render the second step of authentication if first step is incomplete" do
+      request.session["user_id"] = @user.id
+      get :GET_verify_authy
+      response.should redirect_to(root_url)
+    end
+
+    it "should redirect to root_url" do
+      get :GET_verify_authy
+      response.should redirect_to(root_url)
     end
   end
 
-  describe "PUT #update" do
+  describe "POST #verify_authy" do
     it "Should login the user if token is ok" do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      response = mock("authy_request", body: {'status' => 'ok'}.to_json)
-      response.stub(:ok?).and_return(true)
-      Authy::API.should_receive(:verify).with(:id => '80', :token => '567890').and_return(response)
+      request.session["user_id"] = @user.id
+      request.session["user_password_checked"] = true
 
-      put :update, :user => {
-        :authy_id => '80',
-        :token => '567890'
-      }
+      post :POST_verify_authy, :token => '0000000'
+      @user.reload
+      @user.last_sign_in_with_authy.should_not be_nil
       response.should redirect_to(root_url)
       flash.now[:notice].should_not be_nil
     end
 
     it "Shouldn't login the user if token is invalid" do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      response = mock("authy_request", body: {"errors"=>{"token"=>"is invalid"}}.to_json)
-      response.stub(:ok?).and_return(false)
-      Authy::API.should_receive(:verify).with(:id => '80', :token => '567890').and_return(response)
-      put :update, :user => {
-        :authy_id => '80',
-        :token => '567890'
-      }
-      response.should redirect_to(root_url)
+      request.session["user_id"] = @user.id
+      request.session["user_password_checked"] = true
+
+      post :POST_verify_authy, :token => '5678900'
+      response.should render_template('verify_authy')
     end
   end
 
-  describe "GET #register" do
+  describe "GET #enable_authy" do
     it "Should render enable authy view" do
       sign_in @user
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      get :register
-      response.should render_template('register')
+      get :GET_enable_authy
+      response.should render_template('enable_authy')
     end
 
     it "Shouldn't render enable authy view" do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      get :register
+      get :GET_enable_authy
       response.should redirect_to(new_user_session_url)
     end
   end
 
-  describe "POST #create" do
+  describe "POST #enable_authy" do
     it "Should create user in authy application" do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      sign_in @user
-      response = mock("authy_request", body: {'success' => 'ok'}.to_json)
-      response.should_receive(:ok?).and_return(true)
-      response.should_receive(:id).and_return('99')
-      Authy::API.should_receive(:register_user).with(:email => @user.email, :cellphone => '3010008090', :country_code => '57').and_return(response)
+      user2 = create_user
+      sign_in user2
 
-      post :create, :cellphone => '3010008090', :country_code => '57'
+      post :POST_enable_authy, :cellphone => '2222227', :country_code => '57'
+      user2.reload
+      user2.authy_id.should_not be_nil
+      flash.now[:notice].should == "Two factor authentication was enable"
+      response.should redirect_to(user_verify_authy_installation_url)
+    end
 
-      flash.now[:notice].should_not be_nil
-      response.should redirect_to(root_url)
+    it "Should not create user register user failed" do
+      user2 = create_user
+      sign_in user2
+
+      post :POST_enable_authy, :cellphone => '22222', :country_code => "57"
+      response.should render_template('enable_authy')
+      flash[:error].should == "Something went wrong while enabling two factor authentication"
     end
 
     it "Should redirect if user isn't authenticated" do
-      request.env["devise.mapping"] = Devise.mappings[:user]
-      post :create, :user => {
-        :cellphone => '3010008090',
-        :country_code => '57'
-      }
+      post :POST_enable_authy, :cellphone => '3010008090', :country_code => '57'
       response.should redirect_to(new_user_session_url)
+    end
+  end
+
+  describe "GET #verify_authy_installation" do
+    it "Should render the authy installation page" do
+      sign_in @user
+      get :GET_verify_authy_installation
+      response.should render_template('verify_authy_installation')
+    end
+
+    it "Should redirect if user isn't authenticated" do
+      get :GET_verify_authy_installation
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+
+  describe "POST #verify_authy_installation" do
+    it "Should enable authy for user" do
+      sign_in @user
+      post :POST_verify_authy_installation, :token => "0000000"
+      response.should redirect_to(root_url)
+      flash[:notice].should == 'Two factor authentication was enable'
+    end
+
+    it "should not enable authy for user" do
+      sign_in @user
+      post :POST_verify_authy_installation, :token => "0007777"
+      response.should render_template('verify_authy_installation')
+      flash[:error].should == 'Something went wrong while enabling two factor authentication'
+    end
+
+    it "Should redirect if user isn't authenticated" do
+      get :GET_verify_authy_installation
+      response.should redirect_to(new_user_session_url)
+    end
+  end
+
+  describe "POST #request_sms" do
+    it "Should send sms if user is logged" do
+      sign_in @user
+      post :request_sms
+      response.content_type.should == 'application/json'
+      body = JSON.parse(response.body)
+      body['sent'].should be_true
+      body['message'].should == "SMS token was sent"
+    end
+
+    it "Shoul not send sms if user couldn't be found" do
+      post :request_sms
+      response.content_type.should == 'application/json'
+      body = JSON.parse(response.body)
+      body['sent'].should be_false
+      body['message'].should == "User couldn't be found."
     end
   end
 end
