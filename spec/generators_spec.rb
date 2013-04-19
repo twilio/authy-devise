a7463eaddeecea26a1581288e8666a6dd87a80b7
Johanna Mantilla Duque <johanna1431@gmail.com>
2013-04-19 15:47:13 -0500

    Fix specs for generators

--- spec/generators_spec.rb
+++ spec/generators_spec.rb
@@ -14,11 +14,12 @@ describe "generators for devise_authy" do
   end
 
   it "rails g devise_authy:install" do
-    @output = `cd #{RAILS_APP_PATH} && rails g devise_authy:install -p`
-    @output.include?('config/initializers/devise.rb').should be_true
+    @output = `cd #{RAILS_APP_PATH} && rails g devise_authy:install -s`
     @output.include?('config/locales/devise.authy.en.yml').should be_true
-    @output.include?('app/views/devise/devise_authy/register.html.erb').should be_true
-    @output.include?('app/views/devise/devise_authy/show.html.erb').should be_true
+    @output.include?('app/views/devise/devise_authy/enable_authy.html.erb').should be_true
+    @output.include?('app/views/devise/devise_authy/verify_authy.html.erb').should be_true
+    @output.include?('app/views/devise/devise_authy/verify_authy_installation.html.erb').should be_true
     @output.include?('app/assets/stylesheets/devise_authy.css').should be_true
+    @output.include?('app/assets/javascripts/devise_authy.js').should be_true
   end
 end
