ed39e28a9a9e577489a1e746f69e095570b153dc
David A. Cuadrado <krawek@gmail.com>
2014-01-07 16:47:01 -0500

    Correctly return to the previous url.
    Before this commit it wasn't redirecting the user to the correct url
    after signing in.
    
    Fixes #12

--- lib/devise-authy/controllers/helpers.rb
+++ lib/devise-authy/controllers/helpers.rb
@@ -43,6 +43,7 @@ module DeviseAuthy
           id = warden.session(resource_name)[:id]
 
           remember_me = (params.fetch(resource_name, {})[:remember_me].to_s == "1")
+          return_to = session["#{resource_name}_return_to"]
           warden.logout
           warden.reset_session! # make sure the session resetted
 
@@ -50,7 +51,7 @@ module DeviseAuthy
           # this is safe to put in the session because the cookie is signed
           session["#{resource_name}_password_checked"] = true
           session["#{resource_name}_remember_me"] = remember_me
-          session["#{resource_name}_return_to"] = request.path if request.get?
+          session["#{resource_name}_return_to"] = return_to if return_to
 
           redirect_to verify_authy_path_for(resource_name)
           return
