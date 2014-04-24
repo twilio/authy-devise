c8b2daf63cab668fdba738799ae802ce568949c6
Johanna Mantilla Duque <johanna1431@gmail.com>
2014-04-24 16:17:55 -0500

    Configured user model with authy_lockable on authy-devise-demo

--- authy-devise-demo/db/schema.rb
+++ authy-devise-demo/db/schema.rb
@@ -29,6 +29,9 @@ ActiveRecord::Schema.define(:version => 20130409234434) do
     t.string   "authy_id"
     t.datetime "last_sign_in_with_authy"
     t.boolean  "authy_enabled",           :default => false
+    t.integer  "failed_attempts",         :default => 0
+    t.string   "unlock_token"
+    t.datetime "locked_at"
   end
 
   add_index "users", ["authy_id"], :name => "index_users_on_authy_id"
