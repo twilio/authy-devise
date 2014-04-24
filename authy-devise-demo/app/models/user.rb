d51c0c72d5f7ff9f00d9303d9f6f0d2a2bcd5492
Johanna Mantilla Duque <johanna1431@gmail.com>
2014-04-24 16:24:01 -0500

    Update user model of authy-devise-demo

--- authy-devise-demo/app/models/user.rb
+++ authy-devise-demo/app/models/user.rb
@@ -2,8 +2,9 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
   # :lockable, :timeoutable and :omniauthable
-  devise :authy_authenticatable, :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :authy_authenticatable, :authy_lockable, :database_authenticatable,
+          :registerable, :recoverable, :rememberable, :trackable,
+          :validatable, :lockable
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :authy_id, :last_sign_in_with_authy, :email, :password, :password_confirmation, :remember_me
