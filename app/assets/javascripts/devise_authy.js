f543f609bb632e53d2b16747732396f5175429c7
Greg Barendt <greg@barendtenterprises.com>
2015-10-05 21:41:47 -0400

    Trigger JS alert on phone call request

--- app/assets/javascripts/devise_authy.js
+++ app/assets/javascripts/devise_authy.js
@@ -3,5 +3,8 @@ $(document).ready(function() {
   $('a#authy-request-sms-link').bind('ajax:success', function(evt, data, status, xhr) {
     alert(data.message);
   });
+  $('a#authy-request-phone-call-link').bind('ajax:success', function(evt, data, status, xhr) {
+    alert(data.message);
+  });
 });
 
