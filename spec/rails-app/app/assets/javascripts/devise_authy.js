3c0259c640888869cb6a8cc291d82e08b18f1dfc
Johanna Mantilla Duque <johanna1431@gmail.com>
2015-12-14 18:38:02 -0500

    Update Demo and test app with the last changes of the gem.

--- spec/rails-app/app/assets/javascripts/devise_authy.js
+++ spec/rails-app/app/assets/javascripts/devise_authy.js
@@ -3,5 +3,10 @@ $(document).ready(function() {
   $('a#authy-request-sms-link').bind('ajax:success', function(evt, data, status, xhr) {
     alert(data.message);
   });
+
+  $('a#authy-request-phone-call-link').unbind('ajax:success');
+  $('a#authy-request-phone-call-link').bind('ajax:success', function(evt, data, status, xhr) {
+    alert(data.message);
+  });
 });
 
