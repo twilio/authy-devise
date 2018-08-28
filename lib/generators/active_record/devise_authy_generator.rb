45e7acac4d9e460caa48ba622daad4a7f62114fc
Phil Nash <philnash@gmail.com>
2018-08-28 17:59:24 +1000

    Generate versioned database migrations

--- lib/generators/active_record/devise_authy_generator.rb
+++ lib/generators/active_record/devise_authy_generator.rb
@@ -6,7 +6,17 @@ module ActiveRecord
       source_root File.expand_path("../templates", __FILE__)
 
       def copy_devise_migration
-        migration_template "migration.rb", "db/migrate/devise_authy_add_to_#{table_name}.rb"
+        migration_template "migration.rb", "db/migrate/devise_authy_add_to_#{table_name}.rb", migration_version: migration_version
+      end
+
+      private
+
+      def rails5?
+        Rails.version.start_with? '5'
+      end
+
+      def migration_version
+        "[#{Rails::VERSION::MAJOR}.#{Rails::VERSION::MINOR}]" if rails5?
       end
     end
   end
--- lib/generators/active_record/templates/migration.rb
+++ lib/generators/active_record/templates/migration.rb
@@ -1,4 +1,4 @@
-class DeviseAuthyAddTo<%= table_name.camelize %> < ActiveRecord::Migration
+class DeviseAuthyAddTo<%= table_name.camelize %> < ActiveRecord::Migration<%= migration_version %>
   def self.up
     change_table :<%= table_name %> do |t|
       t.string    :authy_id
