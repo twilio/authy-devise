532adfb27f06e117b0da9ae5b02836ad5f6bc5d0
Phil Nash <philnash@gmail.com>
2018-08-29 12:12:13 +1000

    Removes demo app

--- authy-devise-demo/app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ActiveRecord::Base
-  # Include default devise modules. Others available are:
-  # :token_authenticatable, :confirmable,
-  # :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable, :lockable
-end
