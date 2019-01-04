ee38eeed4d2ae1ad73362429ec45041dc07df53d
Peter Dedene <peter@zenjoy.be>
2019-01-04 10:20:50 +0100

    Allow to specify pathnames for request sms or phone call

--- lib/devise-authy/mapping.rb
+++ lib/devise-authy/mapping.rb
@@ -4,6 +4,9 @@ module DeviseAuthy
     def default_controllers(options)
       options[:controllers] ||= {}
       options[:controllers][:passwords] ||= "devise_authy/passwords"
+      options[:path_names] ||= {}
+      options[:path_names][:request_sms] ||= 'request-sms'
+      options[:path_names][:request_phone_call] ||= 'request-phone-call'
       super
     end
   end
--- lib/devise-authy/routes.rb
+++ lib/devise-authy/routes.rb
@@ -16,8 +16,8 @@ module ActionDispatch::Routing
 
       match "/#{mapping.path_names[:authy_onetouch_status]}", :controller => controllers[:devise_authy], :action => :GET_authy_onetouch_status, as: :authy_onetouch_status, via: :get
 
-      match "/request-sms", :controller => controllers[:devise_authy], :action => :request_sms, :as => :request_sms, :via => :post
-      match "/request-phone-call", :controller => controllers[:devise_authy], :action => :request_phone_call, :as => :request_phone_call, :via => :post
+      match "/#{mapping.path_names[:request_sms]}", :controller => controllers[:devise_authy], :action => :request_sms, :as => :request_sms, :via => :post
+      match "/#{mapping.path_names[:request_phone_call]}", :controller => controllers[:devise_authy], :action => :request_phone_call, :as => :request_phone_call, :via => :post
     end
   end
 end
