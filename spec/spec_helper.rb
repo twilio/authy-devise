80db3564a4bf91ac1f56cc787652e22271482d17
Phil Nash <philnash@gmail.com>
2019-01-10 10:36:19 +1100

    Workaround to fix tests in Rails 4 on Ruby 2.6.

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -44,3 +44,18 @@ RSpec.configure do |config|
   #     end
   config.infer_spec_type_from_file_location!
 end
+
+if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+end
