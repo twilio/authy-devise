97455cfd27540f90e9db439f5c63b5ea56020260
Phil Nash <philnash@gmail.com>
2019-01-09 17:58:14 +1100

    Users default_path_names in Mapping instead of default_controllers

--- lib/devise-authy/mapping.rb
+++ lib/devise-authy/mapping.rb
@@ -4,6 +4,10 @@ module DeviseAuthy
     def default_controllers(options)
       options[:controllers] ||= {}
       options[:controllers][:passwords] ||= "devise_authy/passwords"
+      super
+    end
+
+    def default_path_names(options)
       options[:path_names] ||= {}
       options[:path_names][:request_sms] ||= 'request-sms'
       options[:path_names][:request_phone_call] ||= 'request-phone-call'
